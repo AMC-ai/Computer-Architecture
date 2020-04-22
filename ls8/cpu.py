2b0c0c05faf958235354ffd08dfa90879f75541b
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-04-22 01:32:27 -0700

    finished day 1 and day 2 code loading in files dynamically, and was debugging code. Fiinally got 72 my new fav number jk

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,77 @@
 
 import sys
 
+# LDI: load "immediate", store a value in a register, or "set this register to this value".
+LDI = 0b10000010
+# PRN: a pseudo-instruction that prints the numeric value stored in a register.
+PRN = 0b01000111
+HLT = 0b00000001  # HLT: halt the CPU and exit the emulator.
+
+# day 2
+MUL = 0b10100010
+
+
 class CPU:
     """Main CPU class."""
+# day 1 Implement the CPU constructor , Add RAM functions ram_read() and ram_write()
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        # total CPU memory
+        self.ram = [0] * 256
+        # lambda CPU to print 8
+        self.reg = [0] * 8  # r0 - r7
+        # Program Counter, address of the currently executing instruction
+        self.pc = 0
 
-    def load(self):
+    # day 1 should accept the address to read and return the value stored there. The MAR contains the address that is being read or written to. MAR = address = location
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    # day 1 should accept a value to write, and the address to write it to. The MDR contains the data that was read or the data to write. MDR = value
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
+
+    # Later on, you might do further initialization here, e.g. setting the initial value of the stack pointer.
+
+# day 2 Implement the load() function to load an .ls8 file given the filename passed in as an argument
+    def load(self, file):
         """Load a program into memory."""
 
-        address = 0
+        try:
+            with open(file) as f:
 
-        # For now, we've just hardcoded a program:
+                address = 0
+                for line in f:
+                    # split the lines with comments
+                    comments = line.strip().split('#')
+                # take the first element of the line
+                    strings = comments[0].strip()
+                # skip empty lines
+                    if strings == "":
+                        continue
+                # convert the line to an int
+                    int_value = int(strings, 2)
+                # save to memory
+                    self.ram[address] = int_value
+                # increment the address counter
+                    address += 1
+                # then close the file
+                f.close()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {file} not found")
+            sys.exit(2)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+# day 3 math and comparison
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +84,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +96,28 @@ class CPU:
 
         print()
 
+# day 1  Implement the core of run() # day 2 Implement a Multiply instruction (run mult.ls8)
     def run(self):
-        """Run the CPU."""
-        pass
+        # It needs to read the memory address that's stored in register PC, and store that result in IR, the Instruction Register. This can just be a local variable in run()
+        while True:
+            ir = self.ram[self.pc]
+        # Sometimes the byte value is a register number, other times it's a constant value (in the case of LDI). Using ram_read(), read the bytes at PC+1 and PC+2 from RAM into variables operand_a and operand_b in case the instruction needs them.
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if ir == LDI:
+                # set the value of a register to an integer
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif ir == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif ir == MUL:
+                self.alu(ir, operand_a, operand_b)
+                self.pc += 3
+            elif ir == HLT:
+                # exiting the system if HLT is encountered
+                sys.exit(0)
+            else:
+                print(f"Something is not working, what did you forget?")
+                sys.exit(1)
